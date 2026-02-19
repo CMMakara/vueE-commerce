882ccc94f9f51c3d363d2d4c4ebe7f925dba49bb
CMMakara <makaragamer91@gmai.com>
2026-02-19 14:04:50 +0700

    update indec.js

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
+  <header>
+    <nav
+      class="navbar navbar-expand-lg navbar-light bg-light shadow-sm rounded-pill position-fixed top-0 start-50 translate-middle-x mt-3 px-4 py-2"
+      style="z-index: 1000">
+      <a class="navbar-brand fw-bold" href="#">Commerce_</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-  </div>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Collections</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Support</a></li>
+        </ul>
+      </div>
+
+      <div class="d-flex align-items-center gap-2">
+        <button class="btn border-0 bg-transparent"><i class="bi bi-search"></i></button>
+        <button class="btn border-0 bg-transparent"><i class="bi bi-cart"></i> <small>0</small></button>
+        <button class="btn border-0 bg-transparent"><i class="bi bi-person-fill"></i></button>
+      </div>
+      <div class="d-flex align-items-center gap-2">
+        <button class="btn border-0 bg-transparent">login</button>
+        <button class="btn border-0 bg-transparent">Logout</button>
+      </div>
+    </nav>
+  </header>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
+import Navbar from '@/components/layout/navbar.vue'
 import { UseAuthStore } from '@/stores/auth'
 import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
+import CartView from '@/views/cart/CartView.vue'
 import HomeView from '@/views/HomeView.vue'
+import PaymentsView from '@/views/payments/PaymentsView.vue'
+import ProductView from '@/views/products/ProductView.vue'
 import ProfileView from '@/views/profile/ProfileView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -13,17 +18,43 @@ const router = createRouter({
       component : LoginView
     },
     {
+      path : '/register',
+      name : 'register',
+      component : RegisterView
+    },
+    {
       path : '/',
       name : 'home',
       component : HomeView,
-      meta : {requiresAuth : true}
+      // meta : {requiresAuth : true}
     },
     {
       path : '/profile',
       name : 'profile',
       component : ProfileView,
-      meta : {requiresAuth : true}
+      // meta : {requiresAuth : true}
     },
+    {
+      path : '/navbar',
+      name : 'navbar',
+      component : Navbar,
+    },
+    {
+      path : '/cart',
+      name : 'cart',
+      component : CartView,
+    },
+    {
+      path : '/payment',
+      name : 'payment',
+      component : PaymentsView,
+    },
+    {
+      path : '/product',
+      name : 'product',
+      component : ProductView,
+    },
+
   ],
 })
 
