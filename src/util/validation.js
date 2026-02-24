1d444dd32d520d96e87b4ea81e1b4d73ffadcd5b
sman_muslim <smanmuslim8@gmail.com>
2026-02-24 15:54:04 +0700

    validation more

--- src/util/validation.js
+++ src/util/validation.js
@@ -7,6 +7,19 @@ export function isEmail(value , message = 'This field is required'){
     return regex.test(String(value).trim()) ? "" : message;
 }
 
+export function isPassword(value) {
+   const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).+$/;
+   if (!value?.trim()) return 'Password is required';
+   if (!passwordPattern.test(value.trim())) {
+      return 'Password must contain uppercase, lowercase, number and special character';
+   }
+   return '';
+}
+
+export function minlength(value , length , message = `Minimum length is ${length} characters`){
+    return String(value).trim().length >= length ? "" : message;
+}
+
 export function validation(value , rules = []){  
     for(let rule of rules){
         let err = rule(value)
