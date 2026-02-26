a02c2d05c944908c965c87b644700c6c4599a56f
CMMakara <makaragamer91@gmai.com>
2026-02-26 15:01:50 +0700

    fix navbar

--- src/components/layout/navbar.vue
+++ src/components/layout/navbar.vue
@@ -24,31 +24,6 @@
                <button class="btn border-0 bg-transparent text-black me-2">
                   <i class="bi bi-search"></i>
                </button>
-
-<<<<<<< HEAD
-          
-          <router-link :to="{name : 'cart'}" type="button" class="btn border-0 p-0 position-relative bg-transparent me-2">
-            <i class="bi bi-cart fs-4"></i>
-            <span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger" style="top: 10px;"
-            v-if="cartStore.items.length > 0">
-              {{ cartStore.items.length }}
-            </span>
-          </router-link>
-
-          
-          <router-link :to="{name : 'profile'}"  class="btn border-0 bg-transparent text-black" v-if="isCheck">
-            <img
-              src="https://static.vecteezy.com/system/resources/thumbnails/027/951/137/small_2x/stylish-spectacles-guy-3d-avatar-character-illustrations-png.png"
-              alt="" class="bg-black rounded-circle" style="width: 35px; height: 35px;">
-          </router-link>
-          <router-link :to="{name : 'login'}" class="btn btn-info btn-sm text-white fw-bold px-4 rounded" v-else>Login</router-link>
-          <button class="Btn" v-if="isCheck" @click="handleLogout">
-            <div class="sign"><svg viewBox="0 0 512 512">
-                <path
-                  d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z">
-                </path>
-              </svg></div>
-=======
                <button type="button" class="btn border-0 p-0 position-relative bg-transparent me-2">
                   <i class="bi bi-cart fs-4"></i>
                   <span class="position-absolute start-100 translate-middle badge rounded-pill bg-danger"
@@ -70,7 +45,6 @@
                            d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z">
                         </path>
                      </svg></div>
->>>>>>> cc19f1416792906eef391c4f66197120500cb5f7
 
                   <div class="text">Logout</div>
                </button>
@@ -93,14 +67,10 @@
 
 <script setup>
 import { UseAuthStore } from '@/stores/auth';
-<<<<<<< HEAD
-import { useCartStore } from '@/stores/cart';
-import { computed, onMounted} from 'vue';
-=======
-import { computed, ref } from 'vue';
+import { computed, ref ,onMounted } from 'vue';
 import BaseModal from '../ui/BaseModal.vue';
-import { notify } from '@/util/toast';
->>>>>>> cc19f1416792906eef391c4f66197120500cb5f7
+import { notify } from '@/util/toast'; 
+import { useCartStore } from '@/stores/cart';
 
 const auth = UseAuthStore()
 let isCheck = computed(() => auth.isLogin)
