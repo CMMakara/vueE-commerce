c81848ea84229e5ddfea1da3103a60c4df1d130b
borntobehor <hleanghor3@gmail.com>
2026-02-27 11:12:56 +0700

    empty

--- src/components/ui/BaseEmpty.vue
+++ src/components/ui/BaseEmpty.vue
@@ -1,44 +1,38 @@
 <template>
-  <div class=" d-flex justify-content-center align-items-center " style="height: 80vh;">
-    <div class="text-center">
+   <div class=" d-flex justify-content-center align-items-center " style="height: 80vh;">
+      <div class="text-center">
 
-      <div class="mb-3">
-        <img :src="image" alt="empty cart" class="img-fluid opacity-75 " style="width:200px;  filter: drop-shadow(1px 3px 4px rgba(0,0,0,0.5));" />
+         <slot name="img"></slot>
+         <h2 class="fw-semibold mb-2">
+            {{ title }}
+         </h2>
+
+         <p class="text-muted mb-4">
+            {{ subtitle }}
+         </p>
+
+         <RouterLink :to="link" class="btn btn-dark px-4 py-2 rounded-3">
+            {{ buttonText }}
+            <i class="bi bi-arrow-right ms-2"></i>
+         </RouterLink>
+
       </div>
-
-      <h2 class="fw-semibold mb-2">
-        {{ title }}
-      </h2>
-
-      <p class="text-muted mb-4">
-        {{ subtitle }}
-      </p>
-
-      <RouterLink :to="link" class="btn btn-dark px-4 py-2 rounded-3">
-        {{ buttonText }}
-        <i class="bi bi-arrow-right ms-2"></i>
-      </RouterLink>
-
-    </div>
-  </div>
+   </div>
 </template>
 
 <script setup>
 defineProps({
-  title: {
-    type: String,
-  },
-  subtitle: {
-    type: String,
-  },
-  buttonText: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
-  image: {
-    type: String,
-  }
+   title: {
+      type: String,
+   },
+   subtitle: {
+      type: String,
+   },
+   buttonText: {
+      type: String,
+   },
+   link: {
+      type: String,
+   },
 })
 </script>
